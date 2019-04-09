1ebf18df7b9358ec6cebf0f0c2b011eb46272ae0
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-04-09 20:35:39 -0300

    home router added

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,14 @@ const routes = express.Router();
 const BoxController = require('./controllers/BoxController')
 const FileController = require('./controllers/FileController')
 
-routes.post('/boxes', BoxController.store)
 
+routes.post('/boxes', BoxController.store)
 // Upload One File at time, name file.
 routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store)
 routes.get('/boxes/:id', BoxController.show)
 
 routes.get('/', (req, res) => {
-    return res.send("AAAAAAA")
+    return res.send("Home page working")
 })
 
 module.exports = routes
